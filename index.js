e4c6af32b8f9ef122ac76c2520d3a5f09659a18f
Георгий Гришко <57674559+GEOEGII555@users.noreply.github.com>
2022-03-30 20:20:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 function bot() {
 const mineflayer = require('mineflayer')
-
+const express = require('express')
+const app = express()
+const https = require('https')
+const options = {
+  hostname: 'https://banshield-bot.herokuapp.com/',
+  port: 80,
+  path: '/',
+  method: 'GET'
+}
+app.get('/', (req, res) => {
+  res.status(200).send('ok')
+})
+setInterval(function() {
+  req = https.request(options, res => {});
+  req.end();
+}, 5000);
 bot2 = mineflayer.createBot({
-  host: 'hamlet.aternos.host', // minecraft server ip
+  host: 'bichir.aternos.host', // minecraft server ip
   username: 'Bot_BanShield', // minecraft username
   port: 60623,                // only set if you need a port that isn't 25565
   // version: false,             // only set if you need a specific version or snapshot (ie: "1.8.9" or "1.16.5"), otherwise it's set automatically
@@ -22,3 +37,4 @@ bot2.on('kicked', function(e) {bot()})
 bot2.on('error', function(e) {bot()})
 }
 bot();
+app.listen(process.env.PORT, () => {})
